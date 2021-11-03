1c584e9320aa6f9320585e40eaa1cb20c705679e
tessa <[ommitted]>
2021-11-03 01:55:48 -0400

    clean up files

--- test/setup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Chai = require('chai');
-
-const API_KEY = process.env.LOB_API_KEY;
-
-global.expect = Chai.expect;
-global.API_KEY = API_KEY;
-global.Lob = require('../lib/index.js')(API_KEY);
